b6e1bc58429b614f7700407666c22e6121be22df
Marco-Sorrentino <sorrentmarco@gmail.com>
2023-02-08 18:51:37 +0100

    filter added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import BookList from "./component/BookList";
-import InputField from "./component/inputField";
 
 function App() {
   return (
     <div className="App">
-      <InputField />
       <BookList />
     </div>
   );
--- src/component/BookList.jsx
+++ src/component/BookList.jsx
@@ -6,15 +6,36 @@ import SingleBook from "./SingleBook";
 import horror from "../data/horror.json";
 
 class BookList extends Component {
+  // Creazione state in cui andrà dato salvato
+  state = {
+    query: "",
+  };
+  // Creazione funzione, in questo caso metodo che andrà a prendere il valore input
+  filterBookList(ev) {
+    console.log(ev.target.value);
+    this.setState({
+      query: ev.target.value,
+    });
+  }
+
   render() {
     return (
       <Container>
+        <div className="my-5">
+          <input onChange={(event) => this.filterBookList(event)} type="text" />
+        </div>
         <Row>
-          {horror.map((el, index) => (
-            <Col key={index}>
-              <SingleBook img={el.img} title={el.title} price={el.price} />
-            </Col>
-          ))}
+          {horror
+            .filter((book) => {
+              return book.title
+                .toLowerCase()
+                .includes(this.state.query.toLowerCase());
+            })
+            .map((el, index) => (
+              <Col key={index}>
+                <SingleBook img={el.img} title={el.title} price={el.price} />
+              </Col>
+            ))}
         </Row>
       </Container>
     );
